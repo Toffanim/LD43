2d0d770fd01dc435b59e03e00de16cea53db4836
Toffanim <marc.toffanin@gmail.com>
2018-12-01 18:31:09 +0100

    Add colision on weapon

---
+++ LD43/Assets/Weapon.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour {
+
+    BoxCollider2D BC;
+	// Use this for initialization
+	void Start () {
+        BC = GetComponent<BoxCollider2D>();
+	}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        // Do dmg!
+        Debug.Log("Damage!");
+    }
+    // Update is called once per frame
+    void Update () {
+    }
+
+}
