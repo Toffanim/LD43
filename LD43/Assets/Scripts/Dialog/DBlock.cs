a41f29aa0b3fde998bc246062bb6808b7cf401fc
GreenCalx <romain.schoenberger@gmail.com>
2018-12-01 13:05:17 +0100

    Base of dialog system. DBlock/Dialog classes Slopes in level0. @GreenCalx

---
+++ LD43/Assets/Scripts/Dialog/DBlock.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DBlock : MonoBehaviour {
+
+    // ATTRS
+    public string                       message         { get; set; }
+    public Dictionary<string, DBlock>   responses       { get; set; }
+    public bool                         isFinalBlock    { get; set; }
+
+    // Use this for initialization
+    void Start () {
+
+        if (responses!=null )
+        {
+            isFinalBlock = false;
+            DBlock futureBlock = null;
+            foreach (string k in responses.Keys)
+            {
+                responses.TryGetValue(k, out futureBlock);
+                isFinalBlock |= (!!futureBlock);
+                futureBlock = null;
+            }
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ LD43/Assets/Scripts/Dialog/Dialog.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dialog : MonoBehaviour {
+
+    // ATTRS
+    private List<DBlock> dialogChain { get; set; }
+    private DBlock       dialogChainCurrentElem { get; set; }
+    public  DBlock       dialogChainRoot { get; set; }
+
+    public Dialog()
+    {
+        if (!!dialogChainRoot)
+        {
+            dialogChainCurrentElem = dialogChainRoot;
+            dialogChain = new List<DBlock>();
+            dialogChain.Add(dialogChainCurrentElem);
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+
+	}
+
+    public Dictionary<string, DBlock> getChoices()
+    { return dialogChainCurrentElem.responses; }
+	
+    public void makeChoice(string iChoiceKey)
+    {
+        DBlock nextDialog;
+        dialogChainCurrentElem.responses.TryGetValue(iChoiceKey,  out nextDialog);
+        dialogChainCurrentElem = nextDialog;
+    }
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
