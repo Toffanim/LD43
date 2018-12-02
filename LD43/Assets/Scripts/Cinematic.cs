9f707bf056fd50de825884131eb9a5fbc1219558
GreenCalx <romain.schoenberger@gmail.com>
2018-12-02 22:20:55 +0100

    Cinemtics stuff @GreenCalx

---
+++ LD43/Assets/Scripts/Cinematic.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cinematic : MonoBehaviour {
+
+    public int currentCinematicStage { get; set; }
+
+
+    // Use this for initialization
+    void Start () {
+        currentCinematicStage = 0;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ LD43/Assets/Scripts/CinematicFollower.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CinematicFollower : MonoBehaviour {
+
+    public bool paused { get; set; }
+
+    float SmoothSpeed = 0.3f;
+    Transform target;
+    private Vector3 Velocity = Vector3.zero;
+    // Use this for initialization
+    void Start()
+    {
+        target = GameObject.Find("Princess").transform;
+        paused = true;
+    }
+
+    public void setNewTarget(GameObject targetGO)
+    {
+        target = targetGO.transform;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        if (!paused)
+        {
+            Vector3 DesiredPosition;
+            if (target)
+            {
+                Debug.Log("MOVE CAMERA");
+                DesiredPosition = target.position;
+                Vector3 SmoothedPosition = Vector3.SmoothDamp(transform.position, DesiredPosition, ref Velocity, SmoothSpeed);
+                transform.position = new Vector3(SmoothedPosition.x, SmoothedPosition.y, transform.position.z);
+            }
+        }
+    }
+}
