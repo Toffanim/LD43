321043bcc6ea201a64b86fec8aef54691fbb68ef
Toffanim <marc.toffanin@gmail.com>
2018-12-02 02:13:57 +0100

    tentative de flying ennemy

---
+++ LD43/Assets/Scripts/EnnemyState.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnnemyState : MonoBehaviour {
+
+    Rigidbody2D RB2D;
+    public bool IsDamaged = false;
+    int PlayerDamage;
+    int PlayerKnockback;
+    bool WaitForFixUpdate = true;
+    float KnockbackLength = 0.2f;
+    float KnockbackCount = 0f;
+    // Use this for initialization
+    void Start()
+    {
+        RB2D = GetComponent<Rigidbody2D>();
+    }
+
+    public void OnDamage(int Damage, int Knockback)
+    {
+        IsDamaged = true;
+        PlayerDamage = Damage;
+        PlayerKnockback = Knockback;
+        WaitForFixUpdate = true;
+
+        KnockbackCount = KnockbackLength;
+    }
+
+    private void FixedUpdate()
+    {
+        if(IsDamaged)
+             RB2D.AddForce(new Vector2(PlayerKnockback, PlayerKnockback));
+        KnockbackCount -= Time.deltaTime;
+        WaitForFixUpdate = false;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (KnockbackCount <= 0)
+        {
+            IsDamaged = false;
+        }
+	}
+}
---
+++ LD43/Assets/Scripts/FlyingBehavior.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyingBehavior : MonoBehaviour {
+    public Vector2 PlayerDirection;
+    Rigidbody2D RB2D;
+	// Use this for initialization
+	void Start () {
+        RB2D = GetComponent<Rigidbody2D>();
+	}
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        var Player = GameObject.Find("Player 1");
+        if (Player == collision.gameObject)
+        {
+            Vector3 Direction = (Player.transform.position - gameObject.transform.position);
+            Direction.Normalize();
+
+            if (!GetComponent<EnnemyState>().IsDamaged)
+            {
+                RB2D.AddForce(Direction * 80);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+
+    private void FixedUpdate()
+    {
+        RB2D.velocity = (new Vector2(0,0));
+    }
+}
