3efbaf38f5f373f4fa910bc24ec6667e1d6e590d
GreenCalx <romain.schoenberger@gmail.com>
2018-12-03 23:37:13 +0100

    HP UI @GreenCalx

---
+++ LD43/Assets/Scripts/UIHPController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIHPController : MonoBehaviour {
+
+    public GameObject trackedPlayer;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!!trackedPlayer)
+        {
+            PlayerState ps = trackedPlayer.GetComponent<PlayerState>();
+            Text textComp = GetComponent<Text>();
+            if (!!ps && !!textComp)
+            { int val = ps.HP; Debug.Log(val.ToString()); textComp.text = val.ToString(); }
+        }
+	}
+}
