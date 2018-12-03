aa28c41e4360810f628162f558a438e70cf78212
GreenCalx <romain.schoenberger@gmail.com>
2018-12-03 01:36:50 +0100

    Ghosts, heart to regain health and some stuff. Overall mapping @GreenCalx

---
+++ LD43/Assets/Scripts/HPRecovery.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HPRecovery : MonoBehaviour
+{
+    public int healAmount = 50;
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        PlayerController pc = collision.GetComponent<PlayerController>();
+        if (!!pc)
+        {
+            pc.heal(healAmount);
+            Destroy(gameObject);
+        }
+    }
+}
