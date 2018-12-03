7acc21f397cbf2d485646537d4f1df98f2cde74e
Toffanim <marc.toffanin@gmail.com>
2018-12-03 22:32:08 +0100

    BoosBehavior

---
+++ LD43/Assets/Scripts/BossBehavior.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossBehavior : MonoBehaviour {
+    float Speed = 0.01f;
+    float offsetY = 0;
+    GameObject Player;
+	// Use this for initialization
+	void Start () {
+        Player = GameObject.Find("Player 1");
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        var Dir = new Vector3(this.transform.position.x, this.transform.position.y + offsetY, this.transform.position.z) - Player.transform.position ;
+        Dir.Normalize();
+
+        this.transform.position -= Dir * Speed;
+	}
+}
