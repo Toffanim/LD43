8caa07968b7156b7d5adc4748ac8c25b1b8b88ac
GreenCalx <romain.schoenberger@gmail.com>
2018-12-14 20:00:23 +0100

    Sound added Some sprite ideas various little things @GreenCalx

---
+++ LD43/Assets/Scripts/SoundController.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundController : MonoBehaviour {
+
+
+    public AudioSource bgmMusic { get; set; }// NOT MAND
+
+	// Use this for initialization
+	void Start () {
+        bgmMusic = GetComponent<AudioSource>();
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        //bgmMusic = GetComponent<AudioSource>();
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        PlayerController pc = other.GetComponent<PlayerController>();
+        if (!!pc)
+        {
+            playBGM();
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        PlayerController pc = other.GetComponent<PlayerController>();
+        if (!!pc)
+        {
+            pauseBGM();
+        }
+    }
+
+
+    public void playBGM()
+    {
+        if (bgmMusic != null)
+        {
+            bgmMusic.Play(0);
+        }
+    }
+
+    public void pauseBGM()
+    {
+        if (bgmMusic != null)
+            bgmMusic.Pause();
+    }
+
+    public void unpauseBGM()
+    {
+        if (bgmMusic != null)
+            bgmMusic.UnPause();
+    }
+
+
+
+}
